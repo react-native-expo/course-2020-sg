f47271f660668ee49319a34a9482c987ff9132b7
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-19 20:46:02 +0700

    use destructor props

--- c01/rn-starter/src/components/ImageDetail.js
+++ c01/rn-starter/src/components/ImageDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, StyleSheet, View, Image } from 'react-native';
 
-const ImageDetail = (props) => {
+const ImageDetail = ({title, imageSource, score}) => {
     return (
         <View>
-            <Image source={props.imageSource}/>
-            <Text>{props.title}</Text>
-            <Text>Image score - {props.score}</Text>
+            <Image source={imageSource}/>
+            <Text>{title}</Text>
+            <Text>Image score - {score}</Text>
         </View>
     )
 };
