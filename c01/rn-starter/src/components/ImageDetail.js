2d4667135242d1bc46dcd96a1c57270ce502838c
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-19 20:26:58 +0700

    add child component

---
+++ c01/rn-starter/src/components/ImageDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const ImageDetail = () => {
+    return <Text>Image Detail</Text>
+};
+
+const styles = StyleSheet.create({
+});
+
+export default ImageDetail;
