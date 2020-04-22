7ff079e3be195ad950bc4ef8ec50eaab7c84a508
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-22 18:28:07 +0700

    add CounterScreen

---
+++ c01/rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>Current Count:</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default CounterScreen;
