b60141cdc5c21520f31e7a851087a80c1bee220c
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-22 18:40:49 +0700

    useState to store "counter"

--- c01/rn-starter/src/screens/CounterScreen.js
+++ c01/rn-starter/src/screens/CounterScreen.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const CounterScreen = () => {
-  return (
-    <View>
-      <Text>Current Count:</Text>
-    </View>
-  );
+
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <View>
+            <Button title="Increase" onPress={() => setCounter(counter + 1)}></Button>
+            <Button title="Decrease" onPress={() => setCounter(counter - 1)}></Button>
+            <Text>Current Count: {counter}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
