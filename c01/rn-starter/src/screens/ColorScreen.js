4eb936f94aae57fdf5ef32aae7aede1961b97175
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-24 17:09:04 +0700

    chap 6, color app, generate random color

--- c01/rn-starter/src/screens/ColorScreen.js
+++ c01/rn-starter/src/screens/ColorScreen.js
@@ -4,10 +4,20 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 const ColorScreen = ({navigation}) => {
   return (
     <View>
-        <Button title="Add a Color"></Button>      
+        <Button title="Add a Color"></Button>
+
+        <View style={{height: 100, width: 100, backgroundColor: randomRgb()}}></View>
     </View>
   );
 };
+
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
 const styles = StyleSheet.create({});
 
