6a04662818f0532ac864710d4e8bdc22b91cbb3e
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-24 16:53:27 +0700

    chap 6, init Color Screen

---
+++ c01/rn-starter/src/screens/ColorScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const ColorScreen = ({navigation}) => {
+  return (
+    <View>
+        <Button title="Add a Color"></Button>      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
