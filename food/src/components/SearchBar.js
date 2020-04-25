1206158e9c09608adf92cb6c1d486cd1d3e01e85
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-25 23:17:01 +0700

    chap 8, food app, handle input term in Search Bar

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather name='search' style={styles.iconStyle} />
-            <TextInput style={styles.inputStyle} placeholder="Search" />
+            <TextInput
+                autoCapitalize='none'
+                autoCorrect={false}
+                style={styles.inputStyle}
+                placeholder="Search"
+                value={term}
+                // onChangeText={newTerm => onTermChange(newTerm)}
+                // onEndEditing={() => onTermSubmit()} => onEndEditing just word with device
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+            />
         </View>
     )
 };
