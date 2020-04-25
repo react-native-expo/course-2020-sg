e79ac23c54e3c528a678e0f0c7d6e2adb430321d
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-25 22:17:15 +0700

    chap 8, Food app, add navigation
    diff from course, currently using lastest react navigation ver, 5

--- food/App.js
+++ food/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SearchScreen from './src/screens/SearchScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Search"
+        screenOptions= {{
+          title: 'BusinessSearch'
+        }}
+      >
+        <Stack.Screen 
+          name="Search" 
+          component={SearchScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
