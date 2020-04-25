e75b893538b2e2449a35b64795273133bc15500b
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-25 13:01:43 +0700

    chap 7, box screen, Position, Fill object

--- c01/rn-starter/src/screens/BoxScreen.js
+++ c01/rn-starter/src/screens/BoxScreen.js
@@ -26,7 +26,14 @@ const styles = StyleSheet.create({
   textTwoStyle: {
       borderWidth: 3,
       borderColor: 'red',
-      alignSelf: 'flex-end'
+      
+      // fill object
+      // ...StyleSheet.absoluteFillObject
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0
   },
   textThreeStyle: {
       borderWidth: 3,
