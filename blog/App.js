d3e032da89d1444974c0cb60ed13de9e180d785f
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-26 14:48:58 +0700

    c12, Blog, using React.Context
    6,7,8

--- blog/App.js
+++ blog/App.js
@@ -2,26 +2,29 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Index"
-        screenOptions= {{
-          title: 'Blogs',
-          cardStyle: {
-            backgroundColor: 'white'
-          }
-        }}
-      >
-        <Stack.Screen 
-          name="Index" 
-          component={IndexScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Index"
+          screenOptions= {{
+            title: 'Blogs',
+            cardStyle: {
+              backgroundColor: 'white'
+            }
+          }}
+        >
+          <Stack.Screen 
+            name="Index" 
+            component={IndexScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+    const value = useContext(BlogContext);
+
     return (
-        <Text>Index</Text>
+        <View>
+            <Text>Index</Text>
+            <Text>{value}</Text>
+        </View>
     );
 };
 
