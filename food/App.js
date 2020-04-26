9100353f46d856bda430e6729816d156a20209da
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-26 11:42:07 +0700

    c11, Food app, add ResultShow and navigation to it

--- food/App.js
+++ food/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,10 @@ export default function App() {
         <Stack.Screen 
           name="Search" 
           component={SearchScreen}
+        />
+        <Stack.Screen 
+          name="ResultsShow" 
+          component={ResultsShowScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ food/src/screens/ResultsShowScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsShowScreen = () => {
+
+    return (
+        <>
+            <Text>Result</Text>
+        </>
+    )
+};
+
+const styles = StyleSheet.create({
+});
+
+export default ResultsShowScreen;
