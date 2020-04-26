5918f7073ba3d599d90807a566fe387bfa22cf82
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-26 12:21:00 +0700

    c11, Food app, fetch data and styling ResultShowScreen

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ route }) => {
+    
+    const [result, setResult] = useState(null);
     const id = route.params?.id ?? '';
-    console.log(id);
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []); // call only one time when component created
+
+    if (!result) {
+        return null;
+    }
+
     return (
         <>
-            <Text>Result</Text>
+            <Text>{result.name}</Text>
+            <FlatList 
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({item}) => {
+                    return <Image style={styles.image} source={{ uri: item }} />
+                }}
+            />
         </>
     )
 };
 
 const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 300
+    }
 });
 
 export default ResultsShowScreen;
